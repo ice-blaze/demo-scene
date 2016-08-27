bcaf05c51d555dcfef929f8c4db3eb42c43a3f24
Ice-Blaze <franketip@gmail.com>
2016-08-27 10:38:44 +0200

    Play with the code

--- src/shaders/fragment_shader.glsl
+++ src/shaders/fragment_shader.glsl
@@ -8,9 +8,9 @@ const float MAX_DIST = 100.0;
 const float EPSILON = 0.00001;
 
 float sphereSDF(vec3 samplePoint) {
-  vec2 t = vec2(1.0,1.0);
-  vec2 q = vec2(length(samplePoint.xz)-t.x,samplePoint.y);
-  return length(q)-t.y;
+  // vec2 t = vec2(1.0,1.0);
+  // vec2 q = vec2(length(samplePoint.xz)-t.x,samplePoint.y);
+  // return length(q)-t.y;
   return length(samplePoint) - 1.0;
 }
 
@@ -20,7 +20,7 @@ float sceneSDF(vec3 samplePoint) {
 
 float shortestDistanceToSurface(vec3 eye, vec3 marchingDirection, float start, float end) {
     float depth = start;
-    for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
+    for (int i = 0; i < MAX_MARCHING_STEPS; ++i) {
         float dist = sceneSDF(eye + depth * marchingDirection);
         if (dist < EPSILON) {
           return depth;
@@ -96,7 +96,7 @@ vec3 rayDirection(float fieldOfView, vec2 screen_size, vec2 fragCoord) {
 
 void main(void) {
   vec3 dir = rayDirection(145.0, screen_size_out.xy, gl_FragCoord.xy);
-  vec3 eye = vec3(0.0, 0.0, 3.0);
+  vec3 eye = vec3(0.0, 4.0, 3.0);
   float dist = shortestDistanceToSurface(eye, dir, MIN_DIST, MAX_DIST);
 
   // could be an optimisation, but need to be tested
@@ -111,9 +111,11 @@ void main(void) {
   vec3 K_a = vec3(0.2, 0.2, 0.2);
   vec3 K_d = vec3(0.7, 0.2, 0.2);
   vec3 K_s = vec3(1.0, 1.0, 1.0);
+  // vec3 K_s = vec3(0.0, 0.0, 0.0);
   float shininess = 10.0;
 
   vec3 color = phongIllumination(K_a, K_d, K_s, shininess, p, eye);
 
   gl_FragColor = vec4(color, 1.0);
+  // gl_FragColor = vec4(1.0/dist, 0.0, 0.0, 1.0);
 }
