7bca88f4a163eba1c403670f82bcc7593c18b46c
Ice-Blaze <franketip@gmail.com>
2016-08-29 20:45:17 +0200

    Main page which redirect to all projects

--- src/demo_scene/inigo_simple/shaders/fragment_shader.glsl
+++ src/demo_scene/inigo_simple/shaders/fragment_shader.glsl
@@ -1,14 +1,8 @@
+// Code inspired by https://www.shadertoy.com/view/Xds3zN
+
 precision highp float;
 varying vec2 vScreenSize;
 varying float vGlobalTime;
-
-// Created by inigo quilez - iq/2013
-// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
-
-// A list of usefull distance function to simple primitives, and an example on how to
-// do some interesting boolean operations, repetition and displacement.
-//
-// More info here: http://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
 
 float sdPlane( vec3 p )
 {
@@ -163,35 +157,35 @@ vec2 map( in vec3 pos )
     res = opU( vec2( sdPlane(     pos), 1.0 ),res);
     res = opU( res,
 	                vec2( sdSphere(    pos-vec3( 0.0,0.25, 0.0), 0.25 ), 146.9 ) );
-    res = opU( res, vec2( sdBox(       pos-vec3( 1.0,0.25, 0.0), vec3(0.25) ), 3.0 ) );
+    res = opU( res, vec2( sdBox(       pos-vec3( 0.0,.85, 0.0), vec3(0.25) ), 3.0 ) );
     res = opU( res, vec2( udRoundBox(  pos-vec3( 1.0,0.25, 1.0), vec3(0.15), 0.1 ), 41.0 ) );
-	// res = opU( res, vec2( sdTorus(     pos-vec3( 0.0,0.25, 1.0), vec2(0.20,0.05) ), 25.0 ) );
-  //   res = opU( res, vec2( sdCapsule(   pos,vec3(-1.3,0.10,-0.1), vec3(-0.8,0.50,0.2), 0.1  ), 31.9 ) );
-	// res = opU( res, vec2( sdTriPrism(  pos-vec3(-1.0,0.25,-1.0), vec2(0.25,0.05) ),43.5 ) );
-	// res = opU( res, vec2( sdCylinder(  pos-vec3( 1.0,0.30,-1.0), vec2(0.1,0.2) ), 8.0 ) );
-	// res = opU( res, vec2( sdCone(      pos-vec3( 0.0,0.50,-1.0), vec3(0.8,0.6,0.3) ), 55.0 ) );
-	// res = opU( res, vec2( sdTorus82(   pos-vec3( 0.0,0.25, 2.0), vec2(0.20,0.05) ),50.0 ) );
-	// res = opU( res, vec2( sdTorus88(   pos-vec3(-1.0,0.25, 2.0), vec2(0.20,0.05) ),43.0 ) );
-	// res = opU( res, vec2( sdCylinder6( pos-vec3( 1.0,0.30, 2.0), vec2(0.1,0.2) ), 12.0 ) );
-	// res = opU( res, vec2( sdHexPrism(  pos-vec3(-1.0,0.20, 1.0), vec2(0.25,0.05) ),17.0 ) );
-  //
-  //   res = opU( res, vec2( opS(
-	// 	             udRoundBox(  pos-vec3(-2.0,0.2, 1.0), vec3(0.15),0.05),
-	//                  sdSphere(    pos-vec3(-2.0,0.2, 1.0), 0.25)), 13.0 ) );
-  //   res = opU( res, vec2( opS(
-	// 	             sdTorus82(  pos-vec3(-2.0,0.2, 0.0), vec2(0.20,0.1)),
-	//                  sdCylinder(  opRep( vec3(atan(pos.x+2.0,pos.z)/6.2831,
-	// 										  pos.y,
-	// 										  0.02+0.5*length(pos-vec3(-2.0,0.2, 0.0))),
-	// 								     vec3(0.05,1.0,0.05)), vec2(0.02,0.6))), 51.0 ) );
-	// res = opU( res, vec2( 0.7*sdSphere(    pos-vec3(-2.0,0.25,-1.0), 0.2 ) +
-	// 				                   0.03*sin(50.0*pos.x)*sin(50.0*pos.y)*sin(50.0*pos.z),
-  //                                      65.0 ) );
-	// res = opU( res, vec2( 0.5*sdTorus( opTwist(pos-vec3(-2.0,0.25, 2.0)),vec2(0.20,0.05)), 46.7 ) );
-  //
-  //   res = opU( res, vec2(sdConeSection( pos-vec3( 0.0,0.35,-2.0), 0.15, 0.2, 0.1 ), 13.67 ) );
-  //
-  //   res = opU( res, vec2(sdEllipsoid( pos-vec3( 1.0,0.35,-2.0), vec3(0.15, 0.2, 0.05) ), 43.17 ) );
+	res = opU( res, vec2( sdTorus(     pos-vec3( 0.0,0.25, 1.0), vec2(0.20,0.05) ), 25.0 ) );
+    res = opU( res, vec2( sdCapsule(   pos,vec3(-1.3,0.10,-0.1), vec3(-0.8,0.50,0.2), 0.1  ), 31.9 ) );
+	res = opU( res, vec2( sdTriPrism(  pos-vec3(-1.0,0.25,-1.0), vec2(0.25,0.05) ),43.5 ) );
+	res = opU( res, vec2( sdCylinder(  pos-vec3( 1.0,0.30,-1.0), vec2(0.1,0.2) ), 8.0 ) );
+	res = opU( res, vec2( sdCone(      pos-vec3( 0.0,0.50,-1.0), vec3(0.8,0.6,0.3) ), 55.0 ) );
+	res = opU( res, vec2( sdTorus82(   pos-vec3( 0.0,0.25, 2.0), vec2(0.20,0.05) ),50.0 ) );
+	res = opU( res, vec2( sdTorus88(   pos-vec3(-1.0,0.25, 2.0), vec2(0.20,0.05) ),43.0 ) );
+	res = opU( res, vec2( sdCylinder6( pos-vec3( 1.0,0.30, 2.0), vec2(0.1,0.2) ), 12.0 ) );
+	res = opU( res, vec2( sdHexPrism(  pos-vec3(-1.0,0.20, 1.0), vec2(0.25,0.05) ),17.0 ) );
+
+    res = opU( res, vec2( opS(
+		             udRoundBox(  pos-vec3(-2.0,0.2, 1.0), vec3(0.15),0.05),
+	                 sdSphere(    pos-vec3(-2.0,0.2, 1.0), 0.25)), 13.0 ) );
+    res = opU( res, vec2( opS(
+		             sdTorus82(  pos-vec3(-2.0,0.2, 0.0), vec2(0.20,0.1)),
+	                 sdCylinder(  opRep( vec3(atan(pos.x+2.0,pos.z)/6.2831,
+											  pos.y,
+											  0.02+0.5*length(pos-vec3(-2.0,0.2, 0.0))),
+									     vec3(0.05,1.0,0.05)), vec2(0.02,0.6))), 51.0 ) );
+	res = opU( res, vec2( 0.7*sdSphere(    pos-vec3(-2.0,0.25,-1.0), 0.2 ) +
+					                   0.03*sin(50.0*pos.x)*sin(50.0*pos.y)*sin(50.0*pos.z),
+                                       65.0 ) );
+	res = opU( res, vec2( 0.5*sdTorus( opTwist(pos-vec3(-2.0,0.25, 2.0)),vec2(0.20,0.05)), 46.7 ) );
+
+    res = opU( res, vec2(sdConeSection( pos-vec3( 0.0,0.35,-2.0), 0.15, 0.2, 0.1 ), 13.67 ) );
+
+    res = opU( res, vec2(sdEllipsoid( pos-vec3( 1.0,0.35,-2.0), vec3(0.15, 0.2, 0.05) ), 43.17 ) );
 
     return res;
 }
@@ -199,9 +193,9 @@ vec2 map( in vec3 pos )
 vec2 castRay( in vec3 ro, in vec3 rd )
 {
     float tmin = 1.0;
-    float tmax = 20.0;
+    float tmax = 200.0;
 
-	float precis = 0.002;
+	float precis = 0.0002;
     float t = tmin;
     float m = -1.0;
     for( int i=0; i<50; i++ )
@@ -293,7 +287,7 @@ vec3 render( in vec3 ro, in vec3 rd )
 		float spe = pow(clamp( dot( ref, lig ), 0.0, 1.0 ),16.0);
 
         dif *= softshadow( pos, lig, 0.02, 2.5 );
-        dom *= softshadow( pos, ref, 0.02, 2.5 );
+        // dom *= softshadow( pos, ref, 0.02, 2.5 );
 
 		vec3 lin = vec3(0.0);
         lin += 1.20*dif*vec3(1.00,0.85,0.55);
@@ -331,7 +325,7 @@ void main()
 
 	// camera
 	// vec3 ro = vec3( -0.5+3.5*cos(0.1*time + 6.0*mo.x), 10.0 + 2.0*mo.y, 0.5 + 3.5*sin(0.1*time + 6.0*mo.x) );
-	vec3 ro = vec3( -0.5+300.5*cos(0.1*time + 6.0), -1.0 + 2.0, 0.5 + 3.5*sin(0.1*time + 6.0) );
+	vec3 ro = vec3( -0.5+30.5*cos(0.1*time + 6.0), -1.0 + 5.0, 0.5 + 3.5*sin(0.1*time + 6.0) );
 	vec3 ta = vec3( -0.5, -0.4, 0.5 );
 
 	// camera-to-world transformation
