df41bc9d0c3535e64777953ba720bab0e539f7b8
Ice-Blaze <franketip@gmail.com>
2017-01-14 13:35:36 +0100

    Switch space to tabs

--- demo/fractal/javascript/demo_wong.js
+++ demo/fractal/javascript/demo_wong.js
@@ -1,105 +1,105 @@
 const main=function() {
-  const CANVAS=document.getElementById("demo_canvas")
-  const COUNTER=document.getElementById("counter")
-  CANVAS.width=window.innerWidth
-  CANVAS.height=window.innerHeight
+	const CANVAS=document.getElementById("demo_canvas")
+	const COUNTER=document.getElementById("counter")
+	CANVAS.width=window.innerWidth
+	CANVAS.height=window.innerHeight
 
-  let GL
-  try {
-    GL = CANVAS.getContext("experimental-webgl", {antialias: true})
-  } catch (e) {
-    alert("You are not webgl compatible :(")
-    return false
-  }
+	let GL
+	try {
+		GL = CANVAS.getContext("experimental-webgl", {antialias: true})
+	} catch (e) {
+		alert("You are not webgl compatible :(")
+		return false
+	}
 
-  const vertices = [
-    -1.0,-1.0, 0.0,
-     1.0, 1.0, 0.0,
-    -1.0, 1.0, 0.0,
-     1.0,-1.0, 0.0,
-  ]
-  const indices = [0,1,2,0,1,3]
+	const vertices = [
+		-1.0,-1.0, 0.0,
+		 1.0, 1.0, 0.0,
+		-1.0, 1.0, 0.0,
+		 1.0,-1.0, 0.0,
+	]
+	const indices = [0,1,2,0,1,3]
 
-  const get_shader=function(source, type, typeString) {
-    const shader = GL.createShader(type)
-    GL.shaderSource(shader, source)
-    GL.compileShader(shader)
-    if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
-      console.log("ERROR IN "+typeString+ " SHADER : " + GL.getShaderInfoLog(shader))
-      return false
-    }
-    return shader
-  }
+	const get_shader=function(source, type, typeString) {
+		const shader = GL.createShader(type)
+		GL.shaderSource(shader, source)
+		GL.compileShader(shader)
+		if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
+			console.log("ERROR IN "+typeString+ " SHADER : " + GL.getShaderInfoLog(shader))
+			return false
+		}
+		return shader
+	}
 
-  // Link the vertex and fragment shader
-  const vertex_buffer = GL.createBuffer()
-  GL.bindBuffer(GL.ARRAY_BUFFER, vertex_buffer)
-  GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(vertices), GL.STATIC_DRAW)
-  const index_buffer = GL.createBuffer()
-  GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, index_buffer)
-  GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), GL.STATIC_DRAW)
+	// Link the vertex and fragment shader
+	const vertex_buffer = GL.createBuffer()
+	GL.bindBuffer(GL.ARRAY_BUFFER, vertex_buffer)
+	GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(vertices), GL.STATIC_DRAW)
+	const index_buffer = GL.createBuffer()
+	GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, index_buffer)
+	GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), GL.STATIC_DRAW)
 
-  const shader_vertex=get_shader(shader_vertex_source, GL.VERTEX_SHADER, "VERTEX")
-  const shader_fragment=get_shader(shader_fragment_source, GL.FRAGMENT_SHADER, "FRAGMENT")
+	const shader_vertex=get_shader(shader_vertex_source, GL.VERTEX_SHADER, "VERTEX")
+	const shader_fragment=get_shader(shader_fragment_source, GL.FRAGMENT_SHADER, "FRAGMENT")
 
-  const SHADER_PROGRAM=GL.createProgram()
-  GL.attachShader(SHADER_PROGRAM, shader_vertex)
-  GL.attachShader(SHADER_PROGRAM, shader_fragment)
-  GL.linkProgram(SHADER_PROGRAM)
-  GL.useProgram(SHADER_PROGRAM)
+	const SHADER_PROGRAM=GL.createProgram()
+	GL.attachShader(SHADER_PROGRAM, shader_vertex)
+	GL.attachShader(SHADER_PROGRAM, shader_fragment)
+	GL.linkProgram(SHADER_PROGRAM)
+	GL.useProgram(SHADER_PROGRAM)
 
-  // Pass the screen size to the shaders as uniform and quad coordinates as attribute
-  const screen_size_in = GL.getUniformLocation(SHADER_PROGRAM, "screen_size_in")
-  const global_time = GL.getUniformLocation(SHADER_PROGRAM, "global_time_in")
-  const coord = GL.getAttribLocation(SHADER_PROGRAM, "coordinates")
-  var uni_min_re = GL.getUniformLocation(SHADER_PROGRAM, "min_re")
-  var uni_max_re = GL.getUniformLocation(SHADER_PROGRAM, "max_re")
-  var uni_min_im = GL.getUniformLocation(SHADER_PROGRAM, "min_im")
+	// Pass the screen size to the shaders as uniform and quad coordinates as attribute
+	const screen_size_in = GL.getUniformLocation(SHADER_PROGRAM, "screen_size_in")
+	const global_time = GL.getUniformLocation(SHADER_PROGRAM, "global_time_in")
+	const coord = GL.getAttribLocation(SHADER_PROGRAM, "coordinates")
+	var uni_min_re = GL.getUniformLocation(SHADER_PROGRAM, "min_re")
+	var uni_max_re = GL.getUniformLocation(SHADER_PROGRAM, "max_re")
+	var uni_min_im = GL.getUniformLocation(SHADER_PROGRAM, "min_im")
 
-  var min_re = -2.0;
+	var min_re = -2.0;
 	var max_re = 1.5;
 	var min_im = -1.2;
 
-  GL.enableVertexAttribArray(coord)
+	GL.enableVertexAttribArray(coord)
 
-  let time_old=0
-  const counter_list = []
-  let last_mean = 0
-  // main loop
-  const animate=function(time) {
-    // min_re /= 1.01;
-    // max_re /= 1.01;
-    // min_im /= 1.01;
-    let dt=time-time_old
-    counter_list.push(dt)
-    floor_time = Math.floor(time/1000)
-    if( last_mean < floor_time){
-      mean = counter_list.reduce((a, b) => a + b, 0)/counter_list.length;
-      COUNTER.innerHTML = Math.round(mean*100)/100
-      last_mean = floor_time
-      counter_list.length = 0
-    }
-    time_old=time
+	let time_old=0
+	const counter_list = []
+	let last_mean = 0
+	// main loop
+	const animate=function(time) {
+		// min_re /= 1.01;
+		// max_re /= 1.01;
+		// min_im /= 1.01;
+		let dt=time-time_old
+		counter_list.push(dt)
+		floor_time = Math.floor(time/1000)
+		if( last_mean < floor_time){
+			mean = counter_list.reduce((a, b) => a + b, 0)/counter_list.length;
+			COUNTER.innerHTML = Math.round(mean*100)/100
+			last_mean = floor_time
+			counter_list.length = 0
+		}
+		time_old=time
 
-    GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height)
-    GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT)
+		GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height)
+		GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT)
 
-    GL.uniform2f(screen_size_in, CANVAS.width, CANVAS.height)
-    GL.uniform1f(global_time, time/1000)
+		GL.uniform2f(screen_size_in, CANVAS.width, CANVAS.height)
+		GL.uniform1f(global_time, time/1000)
 
-    GL.uniform1f(uni_min_re, min_re)
-    GL.uniform1f(uni_max_re, max_re)
-    GL.uniform1f(uni_min_im, min_im)
+		GL.uniform1f(uni_min_re, min_re)
+		GL.uniform1f(uni_max_re, max_re)
+		GL.uniform1f(uni_min_im, min_im)
 
-    GL.bindBuffer(GL.ARRAY_BUFFER, vertex_buffer)
-    GL.vertexAttribPointer(coord, 3, GL.FLOAT, false, 0, 0)
-    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, index_buffer)
+		GL.bindBuffer(GL.ARRAY_BUFFER, vertex_buffer)
+		GL.vertexAttribPointer(coord, 3, GL.FLOAT, false, 0, 0)
+		GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, index_buffer)
 
-    GL.drawElements(GL.TRIANGLES, indices.length, GL.UNSIGNED_SHORT,0)
+		GL.drawElements(GL.TRIANGLES, indices.length, GL.UNSIGNED_SHORT,0)
 
-    GL.flush()
+		GL.flush()
 
-    window.requestAnimationFrame(animate)
-  }
-  animate(0)
+		window.requestAnimationFrame(animate)
+	}
+	animate(0)
 }
