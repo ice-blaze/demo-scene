20f63bbd1ad140bde1b05193c3271115b7525a43
Ice-Blaze <franketip@gmail.com>
2017-01-16 21:04:30 +0100

    Add color to the fractal.

--- demo/fractal/shaders/fragment_shader.glsl
+++ demo/fractal/shaders/fragment_shader.glsl
@@ -7,9 +7,12 @@ uniform float max_re;
 uniform float min_im;
 uniform float max_im;
 
-const float max_iterations = 16050.0;
+const float max_iterations = 60.0;
 
 void main(void) {
+	const vec3 white = vec3(.25);
+	const vec3 red   = vec3(1., 0., 0.);
+	const vec3 black = vec3(0.);
 	float re_factor = (max_re-min_re)/(vScreenSize.x-1.);
 	float im_factor = (max_im-min_im)/(vScreenSize.y-1.);
 	float c_im = max_im - gl_FragCoord.y*im_factor;
@@ -23,7 +26,9 @@ void main(void) {
 		float z_im2 = z_im*z_im;
 		if(z_re2 + z_im2 > 4.)
 		{
-			gl_FragColor = vec4(vec3(1.-n/max_iterations),1.);
+			vec3 color = mix(black, red, smoothstep(0., max_iterations/2.-1., n));
+			color = mix(color, white, smoothstep(max_iterations/2.-1., max_iterations, n));
+			gl_FragColor = vec4(color, 1.);
 			return;
 		}
 		z_im = 2.*z_re*z_im + c_im;
